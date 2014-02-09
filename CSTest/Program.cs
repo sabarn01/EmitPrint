2bec37d243a0f86dd6a2aba9139e948e3fd5053d
sabarn01 <rerun.adam@gmail.com>
2014-02-09 02:19:24 -0500

    Refactored Methods Lookup

--- CSTest/Program.cs
+++ CSTest/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EmitPrintLib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,20 @@ using System.Threading.Tasks;
 
 namespace CSTest
 {
+    public class foo
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            foo f = new foo{x=4,y = 19 };
+            BaseEmitPrinter Printer = new BaseEmitPrinter();
+            var msg = Printer.EmitPrintMessage(f);
+            Console.WriteLine(msg);
+            var val = Console.ReadLine();
         }
     }
 }
--- EmitPrintLib/BaseEmitPrinter.cs
+++ EmitPrintLib/BaseEmitPrinter.cs
@@ -11,7 +11,8 @@ using System.IO;
 
 namespace EmitPrintLib
 {
-    using oc = OpCodes; 
+    using oc = OpCodes;
+    using EmitPrintLib.KnownMethods; 
     public class BaseEmitPrinter
     {        
         delegate string ObjPrinter(object o,StringBuilder Sb = null);
@@ -48,65 +49,17 @@ namespace EmitPrintLib
             return GeneratedTypes[Typ].Invoke(Obj);
         }
 
-        private static void EmitDebugWriteLine(ILGenerator Gen,string s)
-        {
-            var Fn = typeof(Debug).GetMethod("WriteLine", new Type[]{ typeof(string) } );
-            Debug.Assert(Fn != null);
-            Gen.Emit(OpCodes.Ldstr, s);
-            Gen.Emit(oc.Call, Fn);
-        }
-
         
-
-        private MethodInfo LookupMI(Type T,String Name)
-        {
-            return LookupMI(T, Name, Type.EmptyTypes);
-        }
-
-        private MethodInfo LookupMI(Type T,String Name, Type[] Args )
-        {
-            var MI = T.GetMethod(Name, Args);
-            Debug.Assert(MI != null);
-            return MI; 
-        }
-
         enum Methods
         {
             SBAppendS = 0,
             SBAppendS_O = 1,
             OToString = 2,
         }
-
-        
-
-        MethodInfo[] _MethodMap = null;
-        MethodInfo[] MethodMap
-        {
-            get
-            {
-                if(_MethodMap == null )
-                {
-                    var len = Enum.GetValues(typeof(Methods)).Length;
-                    _MethodMap = new MethodInfo[len]; 
-                    var SBType = typeof(StringBuilder);
-                    //StringBuilder.Append(string)
-                    _MethodMap[(int)Methods.SBAppendS] = LookupMI(SBType, "Append", new Type[] { typeof(string) });
-                    //stringBuilder.Append(string,object)                    
-                    _MethodMap[(int)Methods.SBAppendS_O] = LookupMI(SBType, "AppendFormat", new Type[] { typeof(string), typeof(object) });
-                    //Object.ToString()
-                    _MethodMap[(int)(Methods.OToString)] = LookupMI(typeof(object),"ToString");
-                }
-                return _MethodMap; 
-            }
-        }
-        
-
-     
+             
         private ObjPrinter GenPrinter(Type Typ)
         {
             const string PrintObj = "PrintObj";
-            
-
 #if Gen
             var AsmName = new AssemblyName("Foo");
             var AsmBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(AsmName, AssemblyBuilderAccess.RunAndSave);
@@ -119,15 +72,12 @@ namespace EmitPrintLib
 #endif
             
             var Gen = DYMethod.GetILGenerator();       
-
             HandelPossibleNullPassedInStringBuilder(Gen);
             //Local SB should be the passed in SB or a new one 
             //**********************************************
             GenerateObjectHeader(Typ, Gen);                                    
             GeneratePropertyPrinter(Typ, Gen );
             GenerateFooterPrinter(Gen);
-
-
 #if Gen
             Mod.CreateGlobalFunctions();
             TB.CreateType(); 
@@ -142,8 +92,8 @@ namespace EmitPrintLib
         private void GenerateFooterPrinter(ILGenerator Gen)
         {
             Gen.Emit(OpCodes.Ldstr, "}");
-            Gen.Emit(OpCodes.Call, MethodMap[(int)Methods.SBAppendS] );
-            Gen.Emit(OpCodes.Callvirt, MethodMap[(int)Methods.OToString]);
+            Gen.Emit(OpCodes.Call, StringBuilderMethods.Append_StringMI);
+            Gen.Emit(OpCodes.Callvirt, ObjectMethods.ToStringMI);
             Gen.Emit(OpCodes.Ret);
         }
 
@@ -153,7 +103,6 @@ namespace EmitPrintLib
             foreach (var prop in Typ.GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic))
             {
                 var PropTypeTS = prop.PropertyType.GetMethod("ToString",BindingFlags.Public | BindingFlags.Instance | BindingFlags.InvokeMethod |BindingFlags.DeclaredOnly,null, new Type[] { },null);
-
                 if ( PropTypeTS != null)
                 {
                     var GetMethod = prop.GetGetMethod(false);
@@ -169,9 +118,8 @@ namespace EmitPrintLib
                             Gen.Emit(OpCodes.Ldstr, "," + prop.Name + FmtStr);
                         }
                         FirstPass = false;
-
                         GenPropertyGetter(Gen, prop, GetMethod);
-                        Gen.Emit(OpCodes.Call,MethodMap[(int)Methods.SBAppendS_O] );
+                        Gen.Emit(OpCodes.Call,StringBuilderMethods.Append_Format_ObjectMI);
                     }
                 }
             }
@@ -183,7 +131,7 @@ namespace EmitPrintLib
         {
             Gen.Emit(OpCodes.Ldloc, TheStringBuilder);
             Gen.Emit(OpCodes.Ldstr, Typ.Name + "{");
-            Gen.Emit(OpCodes.Call, MethodMap[(int)Methods.SBAppendS] );
+            Gen.Emit(OpCodes.Call, StringBuilderMethods.Append_StringMI);
         }
 
         protected virtual void HandelPossibleNullPassedInStringBuilder(ILGenerator Gen)
@@ -193,7 +141,7 @@ namespace EmitPrintLib
             TheStringBuilder = SB; 
             var SBIsNotNull = Gen.DefineLabel();
             var JumpOverArg1LclAssign = Gen.DefineLabel();
-            EmitDebugWriteLine(Gen, "IN Dy Methods");
+            EmitUtil.EmitDebugWriteLine(Gen, "IN Dy Methods");
             //Push Arg 1 on stack 
             Debug.Print("Before LDArg_1 offset = " + Gen.ILOffset.ToString());
             Gen.Emit(OpCodes.Ldarg_1);
@@ -216,7 +164,7 @@ namespace EmitPrintLib
             Gen.Emit(OpCodes.Br, JumpOverArg1LclAssign);
             Gen.MarkLabel(SBIsNotNull);
             Debug.Print("SBIsNotNull offset = " + Gen.ILOffset.ToString());
-            EmitDebugWriteLine(Gen, "Arg1 is not null");
+            EmitUtil.EmitDebugWriteLine(Gen, "Arg1 is not null");
             Gen.Emit(OpCodes.Ldarg_1);
             Gen.Emit(OpCodes.Stloc, SB);
             //Stack Should be empty 
